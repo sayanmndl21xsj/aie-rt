5c3f79ba077c7a1e3edb46f6b2d9ccf486821c2a
sayanmndl21xsj <sayanmndl21xsj@github.com>
2024-05-29 18:59:16 -0700

    some changes in xaie debug

--- driver/src/io_backend/ext/xaie_debug.c
+++ driver/src/io_backend/ext/xaie_debug.c
@@ -103,14 +103,13 @@ static AieRC XAie_DebugIO_Init(XAie_DevInst *DevInst)
 * @note		None.
 *
 *******************************************************************************/
-static AieRC XAie_DebugIO_Write32(void *IOInst, u64 RegOff, u32 Value)
-{
-	XAie_DebugIO *DebugIOInst = (XAie_DebugIO *)IOInst;
-
-	printf("W: %p, 0x%x\n", (void *) DebugIOInst->BaseAddr + RegOff, Value);
-
-	return XAIE_OK;
-}
+static AieRC XAie_DebugIO_Write32(void *IOInst, u64 RegOff, u32 Value) {
+	XAie_DebugIO *DebugIOInst = (XAie_DebugIO *)IOInst; 
+	 
+	printf("W: %p, 0x%x\n", (void *) DebugIOInst->BaseAddr + RegOff, Value); 
+	  
+	return XAIE_OK; 
+} 
 
 /*****************************************************************************/
 /**
@@ -299,10 +298,10 @@ static void _XAie_DebugIO_NpiWrite32(void *IOInst, u32 RegOff,
 static AieRC _XAie_DebugIO_NpiMaskPoll(void *IOInst, u64 RegOff, u32 Mask,
 		u32 Value, u32 TimeOutUs)
 {
-	XAie_DebugIO *DebugIOInst = (XAie_DebugIO *)IOInst;
-
-	printf("MP: %p, 0x%x, 0x%x, 0x%d\n", (void *) DebugIOInst->NpiBaseAddr +
-			RegOff, Mask, Value, TimeOutUs);
+	XAie_DebugIO *DebugIOInst = (XAie_DebugIO *)IOInst; 
+	 
+	printf("MP: %p, 0x%x, 0x%x, 0x%d\n", (void *) DebugIOInst->NpiBaseAddr + 
+			RegOff, Mask, Value, TimeOutUs); 
 
 	return XAIE_OK;
 }
